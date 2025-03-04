57a7901e3372b58b700a61407e61952372a648c7
Ayush Rana <124562908+monarchxd33@users.noreply.github.com>
2025-03-04 12:10:20 +0530

    Delete routes directory

--- routes/modelsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getModels, uploadModel } = require("../controllers/modelsController");
-
-const router = express.Router();
-
-router.get("/models", getModels);
-router.post("/upload", uploadModel);
-
-module.exports = router;
